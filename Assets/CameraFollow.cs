0754ca2be815c3a4d33996444145df89078a6be0
Brian Ruggieri <brianruggieri@gmail.com>
2013-05-18 02:46:39 -0400

    update to jumping. added fist to player.

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 	
 	public GameObject player;
-	public float width;
+	public float snapWidth;
+	public float snapHeight;
 	
 	
 	// Use this for initialization
@@ -14,12 +15,20 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(player.transform.position.x > this.transform.position.x+width){
-			this.transform.position = new Vector3(player.transform.position.x - width,this.transform.position.y, this.transform.position.z);	
+		if(player.transform.position.x > this.transform.position.x+snapWidth){
+			this.transform.position = new Vector3(player.transform.position.x - snapWidth,this.transform.position.y, this.transform.position.z);	
 		}
 		
-		if(player.transform.position.x < this.transform.position.x-width){
-			this.transform.position = new Vector3(player.transform.position.x + width,this.transform.position.y, this.transform.position.z);	
+		if(player.transform.position.x < this.transform.position.x-snapWidth){
+			this.transform.position = new Vector3(player.transform.position.x + snapWidth,this.transform.position.y, this.transform.position.z);	
+		}
+		
+		if(player.transform.position.y > this.transform.position.y+snapHeight){
+			this.transform.position = new Vector3(this.transform.position.x ,player.transform.position.y - snapHeight, this.transform.position.z);	
+		}
+		
+		if(player.transform.position.y < this.transform.position.y-snapHeight){
+			this.transform.position = new Vector3(this.transform.position.x ,player.transform.position.y + snapHeight, this.transform.position.z);	
 		}
 	}
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,60 +2,87 @@ using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	
+	public enum FaceDirection { Left  = -1, Right = 1}
+	
+	FaceDirection direction;
 
 	private bool isOnGround;
+	
+	public int speed;
 	
 	public bool IsOnGround{
 		get { return isOnGround; }
 		set { isOnGround = value; }
 		
 	}
-
-	// Use this for initialization
-	void Start () {
 	
+	public GameObject fist;
+	
+	bool wallRight = false;
+	bool wallLeft = false;
+	
+	void Start(){
+		direction = FaceDirection.Right;	
+		fist = gameObject.transform.FindChild("fist").gameObject;
 	}
 	
-	void OnCollisionEnter(Collision collision) {
-		Debug.Log(collision.gameObject.tag);
-        if(collision.gameObject.tag == "platform"){
-			isOnGround = true;
-		}
-        
-    }
-	
-	void OnCollisionExit(Collision collision){
-		Debug.Log(collision.gameObject.tag);
+	void OnCollisionEnter(Collision collision){
 		if(collision.gameObject.tag == "platform"){
-			isOnGround = false;	
+			IsOnGround = true;	
 		}
-			
 	}
 	
-	IEnumerator Jump(){
-		
-		while(this.transform.position.y < this.transform.position.y+1){
-			this.transform.Translate(new Vector3(0, 2* Time.deltaTime, 0));
-			Debug.Log(this.transform.position);
-			yield return null;
+	/*void OnCollisionExit(Collision collision){
+		if(collision.gameObject.tag == "platform"){
+			IsOnGround = false;	
 		}
-		
-	}
+	}*/
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		
 		if(Input.GetKey(KeyCode.RightArrow)){
-			this.transform.Translate(new Vector3(2* Time.deltaTime,0,0));	
-			//this.rigidbody.velocity = new Vector3(2,0,0);
+			
+			direction = FaceDirection.Right;
+			
+			this.transform.Translate(new Vector3((int)direction * speed * Time.deltaTime,0,0));	
+
 		}
 		
 		if(Input.GetKey(KeyCode.LeftArrow)){
-			this.transform.Translate(new Vector3(-2* Time.deltaTime,0,0));	
+			
+			direction = FaceDirection.Left;
+
+			this.transform.Translate(new Vector3((int)direction * speed * Time.deltaTime,0,0));	
+
+		}
+		
+		if(Input.GetKey(KeyCode.DownArrow)){
+			StartCoroutine(Punch());
 		}
 		
 		if(Input.GetKeyDown(KeyCode.Space) && isOnGround){
 			this.rigidbody.velocity = new Vector3(0,-Physics.gravity.y/1.5f,0);
+			IsOnGround = false;
 		}
+	}
+	
+	IEnumerator Punch(){
+		fist.transform.position = new Vector3(this.transform.position.x + this.collider.bounds.extents.x * (int)direction, fist.transform.position.y, fist.transform.position.z);
+		
+		BoxCollider bc = fist.GetComponent<BoxCollider>();
+		
+		while(fist.collider.bounds.size.x <= 1){
+			bc.size = new Vector3(bc.size.x + .3f, bc.size.y, bc.size.z);
+			bc.center =  new Vector3(bc.center.x + .15f * (int)direction, bc.center.y,bc.center.z);
+			yield return null;
+		}
+		
+		yield return new WaitForSeconds(.2f);
+		bc.size = new Vector3(0, bc.size.y, bc.size.z);
+		fist.transform.position = new Vector3(this.transform.position.x + this.collider.bounds.extents.x, fist.transform.position.y, fist.transform.position.z);
+		bc.center = Vector3.zero;
+		
 	}
 }
