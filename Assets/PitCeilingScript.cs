60bae167c576ccb1170e1748afcda739d0d9ef05
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2013-05-18 12:08:05 -0400

    pit ceiling falls after two seconds for the hero

--- Assets/PitCeilingScript.cs
+++ Assets/PitCeilingScript.cs
@@ -6,10 +6,25 @@ public class PitCeilingScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		this.rigidbody.useGravity = false;
+		this.rigidbody.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	 
+	  
+	}
+	
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag == "hero"){
+			StartCoroutine(Fall());	
+		}
+	}
+	
+	
+	IEnumerator Fall(){
+		yield return new WaitForSeconds(2);
+		
+		this.rigidbody.isKinematic = false;
+		this.rigidbody.useGravity = true;	
 	}
 }
