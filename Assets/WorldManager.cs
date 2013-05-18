1dd705d6de5afecd2ddb7ad142c1ebede70eb961
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2013-05-18 14:51:03 -0400

    track scores for characters

---
+++ Assets/WorldManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class WorldManager : MonoBehaviour {
+	
+	// declare hero
+	// declare benedict
+	public int hero_score;
+	public int benedict_score;
+	
+	// Use this for initialization
+	void Start () {
+		// set up hero
+		
+		// set up score
+		hero_score = 0;
+		benedict_score = 0;
+		
+		// set up anything else
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
